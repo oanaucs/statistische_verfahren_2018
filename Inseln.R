5007b63eeba164e45f86d93e012326f18a555bc8
Selina Müller <selmueller42@gmail.com>
2018-08-03 11:21:40 +0200

    R-Projekt angelegt
    - R Projekt angelegt
    - Rplot: "Übersicht"
    - Inseln.R ausgewählte Plots

---
+++ Inseln.R
@@ -0,0 +1,16 @@
+########   Blattläuse auf Inseln P3 ##########
+
+data = read.csv('/mnt/5A71DF116192D4FC/Dokumente/R/islands.csv')
+laeuse = data$mf.presence + data$mt.presence
+plot(data$no.ramet, laeuse)
+plot(data$size, laeuse) # makes sense
+plot(data$dist.isl.MF, data$mf.presence)
+plot(data$dist.isl.MF, data$mt.presence)
+plot(data$no.hab, laeuse) # useable
+plot(data$dens.group, laeuse) #useable
+plot(data$no.ramet, data$no.group)
+# histogramm für trees und läuse
+plot(data$mean.height, laeuse) # useable
+plot(data$dist.group, laeuse) #useable
+plot(data$mt.presence, data$mf.presence) # anders plotten
+
